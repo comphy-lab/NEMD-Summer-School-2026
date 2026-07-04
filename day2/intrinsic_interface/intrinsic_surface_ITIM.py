be38ee980705d887f6fc6bd5da1d1bddb24c1674
Marcello Sega <marcello.sega@gmail.com>
2026-07-04 12:28:13 +0100

    Update default values for intrinsic_surface_ITIM
    so that students don't need bother

--- day2/intrinsic_interface/intrinsic_surface_ITIM.py
+++ day2/intrinsic_interface/intrinsic_surface_ITIM.py
@@ -3,9 +3,9 @@
 import argparse
 p = argparse.ArgumentParser()
 p.add_argument("trajectory", nargs="?", default="day2_traj.lammpstrj")
-p.add_argument("-a", "--alpha", type=float, default=0.8)
+p.add_argument("-a", "--alpha", type=float, default=0.6)
 p.add_argument("-l", "--layers", type=int, default=5)
-p.add_argument("--start", type=int, default=0)
+p.add_argument("--start", type=int, default=100)
 p.add_argument("--stop", type=int, default=-1)
 p.add_argument("--stride", type=int, default=1)
 p.add_argument("-o", "--output", default="itim")
